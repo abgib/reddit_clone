843172af45b1f744c7bf7324fa42b620f29226d9
JohnSteve <John_Steve@appacademy.io>
2015-06-19 16:10:27 -0400

    Post#update, #edit. Post edit view.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,22 @@ class PostsController < ApplicationController
     end
   end
 
-  private
+  def edit
+    @post = Post.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :edit
+    end
+  end
+  
+private
   def post_params
     params.require(:post).permit(
             :title,
